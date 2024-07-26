11239b7f1dcc0214626fe356e5ea80e94d67a68e
Junsik0408 <lccdc@naver.com>
2024-07-26 18:47:06 +0900

    Code_0011

---
+++ src/main/java/Code_0011.java
@@ -0,0 +1,11 @@
+public class Code_0011 {
+    public static void main(String[] args) {
+        int num = 200;
+
+        if (num < 100)
+            System.out.println("100보다 작군요.");
+
+        else
+            System.out.println("100보다 크군요");
+    }
+}
